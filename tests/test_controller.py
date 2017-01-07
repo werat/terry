3ea1ca29d8cb18cc5bf0a046dcac27967a99d14e
Andy Yankovsky <weratt@gmail.com>
2017-01-07 21:53:33 +0300

    Fix flapping test

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -45,9 +45,9 @@ def test_reacquire_job(controller):
     job_1 = controller.acquire_job(['test-tag'], 'test-worker')
     assert job_1.id == job_id
 
-    from datetime import datetime
-    controller._update_job(job_1.id, job_1.version,
-                           worker_heartbeat=datetime.utcnow() - controller.HEARTBEAT_TIMEOUT)
+    from datetime import datetime, timedelta
+    fake_heartbeat = datetime.utcnow() - controller.HEARTBEAT_TIMEOUT - timedelta(minutes=1)
+    controller._update_job(job_1.id, job_1.version, worker_heartbeat=fake_heartbeat)
 
     job_2 = controller.acquire_job(['test-tag'], 'test-worker')
     assert job_2.id == job_id
